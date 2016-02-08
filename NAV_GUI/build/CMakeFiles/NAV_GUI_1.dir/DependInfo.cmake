0447f45060fc6ac223b06111c4cc6b110c641151
kyle <kylejbrown17@gmail.com>
2016-02-08 10:10:02 -0700

    First commit

---
+++ NAV_GUI/build/CMakeFiles/NAV_GUI_1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kyle/gitMars/NAV_GUI/NAV_GUI/display.cpp" "/home/kyle/gitMars/NAV_GUI/NAV_GUI/build/CMakeFiles/NAV_GUI_1.dir/display.cpp.o"
+  "/home/kyle/gitMars/NAV_GUI/NAV_GUI/main.cpp" "/home/kyle/gitMars/NAV_GUI/NAV_GUI/build/CMakeFiles/NAV_GUI_1.dir/main.cpp.o"
+  "/home/kyle/gitMars/NAV_GUI/NAV_GUI/patch.cpp" "/home/kyle/gitMars/NAV_GUI/NAV_GUI/build/CMakeFiles/NAV_GUI_1.dir/patch.cpp.o"
+  "/home/kyle/gitMars/NAV_GUI/NAV_GUI/rover.cpp" "/home/kyle/gitMars/NAV_GUI/NAV_GUI/build/CMakeFiles/NAV_GUI_1.dir/rover.cpp.o"
+  "/home/kyle/gitMars/NAV_GUI/NAV_GUI/screenshot.cpp" "/home/kyle/gitMars/NAV_GUI/NAV_GUI/build/CMakeFiles/NAV_GUI_1.dir/screenshot.cpp.o"
+  "/home/kyle/gitMars/NAV_GUI/NAV_GUI/waypoint.cpp" "/home/kyle/gitMars/NAV_GUI/NAV_GUI/build/CMakeFiles/NAV_GUI_1.dir/waypoint.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/ros/indigo/include/opencv-3.1.0-dev/opencv"
+  "/opt/ros/indigo/include/opencv-3.1.0-dev"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ NAV_GUI/build/CMakeFiles/NAV_GUI_1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/NAV_GUI_1.dir/main.cpp.o"
+  "CMakeFiles/NAV_GUI_1.dir/rover.cpp.o"
+  "CMakeFiles/NAV_GUI_1.dir/waypoint.cpp.o"
+  "CMakeFiles/NAV_GUI_1.dir/patch.cpp.o"
+  "CMakeFiles/NAV_GUI_1.dir/screenshot.cpp.o"
+  "CMakeFiles/NAV_GUI_1.dir/display.cpp.o"
+  "NAV_GUI_1.pdb"
+  "NAV_GUI_1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/NAV_GUI_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
